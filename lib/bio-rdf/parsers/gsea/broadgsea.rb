089375d617a6eb0ec4fcb8c26b893d83dccf7bb9
Pjotr Prins <pjotr.public01@thebird.nl>
2012-05-01 08:41:13 +0200

    Broad GSEA CLS parser

--- lib/bio-rdf/parsers/gsea/broadgsea.rb
+++ lib/bio-rdf/parsers/gsea/broadgsea.rb
@@ -2,9 +2,11 @@ module BioRdf
   module Parsers
     module BroadGSEA
       class ParseClsRecord
+        attr_reader :classnames, :classes
         def initialize buf
+          lines = buf.split("\n")
+          p lines
         end
-
       end
     end
   end
