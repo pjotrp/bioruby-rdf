c68b89ecd4587c3568dcf91ad4f494d5e8759ffa
Pjotr Prins <pjotr.public01@thebird.nl>
2013-10-07 11:20:59 +0200

    Parsing

--- lib/bio-rdf/extra/gwp.rb
+++ lib/bio-rdf/extra/gwp.rb
@@ -3,8 +3,18 @@ module BioRdf
     module Extra
       module GWP
         def GWP::parse_digest(buf)
-          lines = string.split(/\n/).map { |s| s.split(/\s+/) }
-          lines
+          lines = buf.split(/\n/).map { |s| s.split(/\s+/) }
+          recs = {}
+          lines.each do | a |
+            cluster = /(cluster\d+)/.match(a[0])[1]
+            r = recs[cluster] = {}
+            r[:model] = a[1] 
+            r[:lnL] = a[4].to_f
+            r[:is_pos_sel] = (a[5] == '++')
+            r[:sites] = a[6].to_i
+            r[:seq_size] = a[8][1..-1].to_i if a[8] 
+          end
+          recs
         end
       end
     end
