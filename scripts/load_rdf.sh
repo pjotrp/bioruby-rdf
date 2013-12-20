4d93c316264d9a433a5d2e3e3da38023545337ad
Pjotr Prins <pjotr.public01@thebird.nl>
2013-12-20 09:49:27 +0100

    Load script

--- scripts/load_rdf.sh
+++ scripts/load_rdf.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-rdf=$1
-# ---- test syntax
-rapper -i turtle $rdf > /dev/null
+for rdf in $* ; do
+  echo "Loading $rdf"
+  # ---- test syntax
+  rapper -i turtle $rdf > /dev/null
 
-uri=http://localhost:8000/data/http://biobeat.org/data/$rdf
+  uri=http://localhost:8000/data/http://biobeat.org/data/$rdf
 
-curl -X DELETE $uri
-curl -T $rdf -H 'Content-Type: application/x-turtle' $uri
-
+  curl -X DELETE $uri
+  curl -T $rdf -H 'Content-Type: application/x-turtle' $uri
+done
 
