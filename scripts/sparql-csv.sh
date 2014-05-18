fc1ef346c4b7da067e347fc1e334231dc6ca7087
Pjotr Prins <pjotr.public01@thebird.nl>
2014-05-18 20:09:05 +0200

    csv

---
+++ scripts/sparql-csv.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+sparql=$1
+shift
+
+erb $sparql | ~/opt/bin/sparql-query "http://localhost:8000/sparql/?soft-limit=-1" -p -n |xalan -xsl $HOME/izip/git/opensource/ruby/bioruby-rdf/scripts/sparql-results-csv.xsl
+
