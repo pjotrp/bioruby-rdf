240ac867f4d241b8637ed2c73f988ce94f3befc7
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-21 09:49:16 +0200

    SPARQL

--- scripts/sparql-csv.sh
+++ scripts/sparql-csv.sh
@@ -3,5 +3,5 @@
 sparql=$1
 shift
 
-erb $sparql | ~/opt/bin/sparql-query "http://localhost:8000/sparql/?soft-limit=-1" -p -n |xalan -xsl $HOME/izip/git/opensource/ruby/bioruby-rdf/scripts/sparql-results-csv.xsl
+erb $sparql | ~/opt/bin/sparql-query "http://localhost:8000/sparql/?soft-limit=-1" -p -n |xalan -q -xsl $HOME/izip/git/opensource/ruby/bioruby-rdf/scripts/sparql-results-csv.xsl
 
