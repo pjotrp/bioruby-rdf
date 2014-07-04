b003a99d21e0158f249040da5de521fe613dda62
Pjotr Prins <pjotr.public01@thebird.nl>
2014-07-04 12:17:37 +0200

    script

--- sparql/extra/gwp/gen_table2.rb
+++ sparql/extra/gwp/gen_table2.rb
@@ -4,6 +4,9 @@
 #
 #
 require 'csv'
+require 'solid_assert'
+
+SolidAssert.enable_assertions
 
 TYPE = if ARGV[0] == 'DNA'
          'DNA'
@@ -35,7 +38,7 @@ catB1 = csv_parse.call("env HASH=\"clusters=1,species=Mi,source1=#{TYPE}\" ../..
 # ---- 2b. Now we have the unique PSC (catC green) (&)
 minc_cluster_unique = minc_psc - catB1
 p [:unique_PSC, minc_cluster_unique.size]
-raise "Error" if TYPE=='CDS' and minc_cluster_unique.size != 7
+assert(minc_cluster_unique.size == 7) if TYPE=='CDS'
 
 # ---- 2c. Annotate plantP only (&)
 #      CatB1 contains all ann PSC. So we can select those that
@@ -54,11 +57,14 @@ matches = list1.inject(Hash.new) { |h,a|
 ann = {}
 
 matches.each { |cluster,ms| 
-  plant = false
-  patho = false
-  free  = false
-  other = false
+  plant  = false
+  patho  = false
+  free   = false
+  other  = false
+
   ms.keys.each { | s |
+    assert(s != 'Mi')  # Make sure there is no accidental self reference
+    assert(s != 'Nema')  # Make sure there is no accidental self reference
     if PLANT_PATHOGENS.include?(s)
       plant = true 
     elsif ANIMAL_PATHOGENS.include?(s)
@@ -80,38 +86,50 @@ matches.each { |cluster,ms|
     ann[cluster] << :pathogen
     ann[cluster] << :plant_pathogen
   end
+
+  cds    = false
+  dna    = false
+  refseq = false
+
+  ms.keys.each { | s |
+    full = ms[s]
+    if full =~ /_CDS/
+      cds = true
+    elsif full =~ /_DNA/
+      dna = true
+    else 
+      refseq = true
+    end
+  }
+  ann[cluster] << :refseq if refseq 
+  ann[cluster] << :cds if cds
+  ann[cluster] << :dna if dna
 }
+p [:annotated, ann]
 
 minc_cluster_plantp = ann.keys.select { |k| ann[k].include?(:plant_pathogen) }
 p [:plantP, minc_cluster_plantp.size ]
-raise "Error" if TYPE=='CDS' and minc_cluster_plantp.size != 9
-exit
+assert(minc_cluster_plantp.size == 9) if TYPE=='CDS' 
 
 # ---- 2d and 2e. Annotate is in ann (&)
 
 # ---- 3. Fetch matching PSC (catA).
-#      Cat. A - create pairs of cluster + species
+#      Cat. A - create pairs of cluster + species, the list may contain
+#      references to other Mi EST clusters, but not to self
 listA = csv_parse.call("env HASH=\"species1=Mi,is_pos_sel=1,source1=#{TYPE},species2=Other,is_pos_sel2=1\" ../../../scripts/sparql-csv.sh match_clusters.rq").drop(1)
 
-p listA
+# p listA
+
+# ---- 3a catA plant only
+red_plant_pathogenA = listA.map{ |pair| pair[0] }.select { |c| ann[c] and ann[c].include?(:plant_pathogen) }
+orange_planthogenA = listA.map{ |pair| pair[0] }.select { |c| ann[c] and ann[c].include?(:pathogen) }
+
+# ---- Fetch conserved (catB)
+
 
 
 exit # Old old old...
 
-exit
-
-# Create hash of clusters with matching species
-catA = listA.drop(1).inject(Hash.new) { |h,pair| 
-  h[pair[0]] ||= []
-  h[pair[0]] << pair[1] 
-  h
-}
-total_catA = catA.size
-p catA
-p [:catA_PSC_PSC,total_catA]
-raise "Error" if TYPE=='CDS' and total_catA!=10
-
-exit
 
 # ---- 1b. Get the other PSC
 count_pos = csv_parse.call("env count=1 ../../../scripts/sparql-csv.sh count_pos_sel.rq")
@@ -124,75 +142,4 @@ total = type_count_pos[:Mi]
 p [:total,total] 
 raise "Error" if TYPE=='CDS' and total!=43
 
-  # ---- subcat A1 count PlantP only
-  subcatA = catA.keys.map { |cluster| 
-    species = catA[cluster]
-    p species
-    species.inject(true) { |cnt,s|
-      (!PLANT_PATHOGENS.include?(s) ? false : cnt )
-    }
-  }
-  p [:subcatA,subcatA.count(true)]
-
-# Not quite done yet, we need to subtract the ones that have matches in catB
-# as they are not totally unique!
-
-# ---- Cat. B
-  total_catB = catB1.size-1 - total_catA
-  p [:catB,total_catB]
-  cnames = catB1.flatten[1..-1]
-  # p cnames
-  cluster = {}
-  cnames.map { |c| cluster[c] = [] }
-  catB = csv_parse.call("env HASH=\"by_gene=1,species=Mi,source1=#{TYPE}\" ../../../scripts/sparql-csv.sh blast2.rq")[1..-1].map {|row| cluster[row[0]] << row[2] }
-
-  # subcategorise B, first split into plantp, non-plantp, refseq, and dna
-  #
-  # The idea is to first find the Refseq matches, next the TYPE and
-  # finally the DNA.
-  wormp = []; worm = []; refseq=[]; orf=[]
-  cluster.each do | cname, species |
-    # Look for CDS pathogens ann
-    non_pathogen = nil
-    matched = nil
-    species.each do | s |
-      if (match = /(\S\S)_CDS/.match(s))
-        matched = true
-        if not PLANT_PATHOGENS.include?(match[1])
-          non_pathogen = true 
-        end
-      end
-    end
-    if matched
-      if non_pathogen
-        worm << cname
-      else
-        wormp << cname
-      end
-      next # cluster
-    end
-    next
-    # Look for RefSeq ann or DNA
-    is_orf = false
-    matched = false
-    species.each do | s |
-      if (match = /(\S\S)_DNA/.match(s))
-        matched = true
-        is_pathogen = true if PLANT_PATHOGENS.include?(match[1])
-      end
-    end
-    if matched
-      if is_pathogen
-        wormp << cname
-      else
-        worm << cname
-      end
-      next # cluster
-    end
-  end
-  p wormp
-  p worm
-
-  exit
-  
 
